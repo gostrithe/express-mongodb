3feaf5c6c66a4dbdb9d2e6e066145660fddb0973
gostrithe <11775651+gostrithe@user.noreply.gitee.com>
2022-10-24 02:39:30 +0800

    7

---
+++ rooter/process_post.js
@@ -0,0 +1,14 @@
+const process_post = (req, res) => {
+    const response = {
+        "first_name": req.query.first_name,  // 与表单上的name有关，绑定name拿到value
+        "last_name": req.query.last_name
+    };
+    console.log(response);
+
+    res.end(JSON.stringify(response));
+    // res.json(response)
+};
+
+module.exports = {
+    process_post
+};
